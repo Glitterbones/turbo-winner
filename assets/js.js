bf46b92e2bc4eca5bb6ec8bce31701095e6b0ea4
ashley lockhart <allockhart49@gmail.com>
2022-03-22 21:21:53 -0600

    questions

---
+++ assets/js.js
@@ -0,0 +1,24 @@
+var questions = [
+    {
+      id: 1,
+      question: "Where was Buddha born?",
+      answers: [
+        { text: "A: Japan", correct: false },
+        { text: "B: Napal", correct: true },
+        { text: "C: Thailand", correct: false },
+        { text: "D: India", correct: false },
+      ],
+    },
+  
+    {
+        id: 2,
+        question: "What was Buddha orinally born?",
+        answers: [
+          { text: "A: An Orphan", correct: false },
+          { text: "B: A monk", correct: false },
+          { text: "C: Into a working class family ", correct: false },
+          { text: "D: a Wealthy prince", correct: true },
+        ],
+      },
+
+  ];
