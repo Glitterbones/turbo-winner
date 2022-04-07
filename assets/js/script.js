fa434aa8a989d7a3c5bf23090c2916a6e04369e7
ashley lockhart <allockhart49@gmail.com>
2022-04-06 19:49:17 -0600

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,27 @@
-var questionCard = document.querySelector("#question-card")
-var resultsCard = document.querySelector("#results-card")
 var startButton = document.querySelector(".start-btn");
 var nextButton = document.querySelector(".next-btn");
 var startCard = document.querySelector("#start-card");
+var questionCard = document.querySelector("#question-card")
+var resultsCard = document.querySelector("#results-card")
 var optButton = document.querySelectorAll(".btn")
 var question = document.querySelector("#question")
 var choices = document.querySelector("#choices");
-var option1 = document.querySelector("#Answer A");
-var option2 = document.querySelector("#Answer B");
-var option3 = document.querySelector("#Answer C");
-var option4 = document.querySelector("#Answer D");
+var option1 = document.querySelector("#AnswerA");
+var option2 = document.querySelector("#AnswerB");
+var option3 = document.querySelector("#AnswerC");
+var option4 = document.querySelector("#AnswerD");
 var currentTime = document.querySelector("#current-time");
 var timeCount = document.querySelector(".timer");
-var qindex = 0
+var saveScore = document.querySelector("#save-btn");
 var points = 0
-var timer = 30
-
-
-
+var qindex = 0
+var timer = 60
 
 
 var questions = [
 {
         title: "Where was Buddah born?",
-        choiceA: ["Japan", "Nepal", "Thailand ", "india"],
+        choiceA: ["Japan", "Nepal", "Thailand ", "india", ],
         answer: "Nepal"
     },
 
@@ -48,19 +46,24 @@ var questions = [
 ]
   
 
-startButton.addEventListener("click", startQuiz) 
+// Start quiz
+startButton.addEventListener("click", startQuiz)
+
 
 function startQuiz() {
-  nextQuestion()
-  startCounter(30);
+    startCard.style.display = "none"
+    questionCard.style.display = "block"
+    nextQuestion()
+    startCounter(60);
 }
 
+
 function nextQuestion() {
-  question.textContent = questions[qindex].title
-  AnswerA.textContent = questions[qindex].choiceA[0]
-  AnswerB.textContent = questions[qindex].choiceA[1]
-  AnswerC.textContent = questions[qindex].choiceA[2]
-  AnswerD.textContent = questions[qindex].choiceA[3]
+    question.textContent = questions[qindex].title
+    option1.textContent = questions[qindex].choiceA[0]
+    option2.textContent = questions[qindex].choiceA[1]
+    option3.textContent = questions[qindex].choiceA[2]
+    option4.textContent = questions[qindex].choiceA[3]
 
 }
 
@@ -79,17 +82,24 @@ optButton.forEach(button => {
     })
 })
 
-function startCounter(seconds) {
-  let timeCount = seconds;
 
-  const timer = setInterval(() => {
-      console.log(timeCount);
-      timeCount--;
-      currentTime.textContent = "Time:" + timeCount;
-      if (timeCount === 0) {
-          clearInterval(timer);
-          alert("Time's up!")
-          location.href = "./highscore.html"
-      }
-  }, 1000)
+function startCounter(seconds) {
+    let timeCount = seconds;
+
+    const timer = setInterval(() => {
+        console.log(timeCount);
+        timeCount--;
+        currentTime.textContent = "Time:" + timeCount;
+        if (timeCount === 0) {
+            clearInterval(timer);
+            alert("END!")
+            location.href = "./highscore.html"
+        }
+    }, 1000)
 }
+
+
+
+
+
+
